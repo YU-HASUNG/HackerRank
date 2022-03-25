497d02da09967f74bb532b62c09314ca20fb8c5c
hasung <dbgktjd@naver.com>
2022-03-25 09:49:06 +0900

    HackerRank Lonely Integer

---
+++ 1Week_Preparation_Kit/Lonely_Integer/Solution.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int lonelyinteger(List<Integer> a) {
+    // Write your code here
+
+    boolean[] arr = new boolean[a.size()];
+    int cnt = 0;
+
+    for(int i=0;i<a.size();i++){
+        for(int j=0;j<a.size();j++){
+            if(a.get(i) == a.get(j) && i!=j){
+                arr[i] = true;
+                continue;
+            }
+        }
+    }
+
+    for(int i=0;i<a.size();i++){
+        if(arr[i]==false){
+            cnt = a.get(i);
+        }
+    }
+
+    return cnt;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.lonelyinteger(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
